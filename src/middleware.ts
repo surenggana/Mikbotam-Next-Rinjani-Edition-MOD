998db83ada438ef39f180a64fc072f0904fbea85
zxan <zxan@192.168.1.6>
2026-04-24 15:04:48 +0800

    chore: remove default logos and unify favicon to official logo

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,5 @@ import { authConfig } from "./auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|logo-next.svg|logo-outline.svg).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|logo-outline.svg).*)'],
 }
